9a57799bc1aa1062f980e06d413b99fbd9636a92
Kavindi487 <kavindividusari@gmail.com>
2025-07-13 21:26:58 +0530

    updated dashboard and logout functionality

--- backend/backend/app/Http/Controllers/UsersController.php
+++ backend/backend/app/Http/Controllers/UsersController.php
@@ -72,32 +72,35 @@ class UsersController extends Controller
 
     public function dashboard(Request $request)
     {
-        // Validate request data
-        $request->validate([
-            'email' => 'required|email',
-            'password' => 'required|string|min:8|max:12',
-        ]);
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+        } catch (\Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+            return response()->json(['error' => 'Token Invalid'], 401);
+        } catch (\Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+            return response()->json(['error' => 'Token Expired'], 401);
+        } 
 
-       $user = User::where('email', $request->email)->first();
-
-       if($user){
-        return response()->json([
-            'error' => 'Invalid Email'], 401);
-       }
-       elseif(!Hash::check($request->password, $user->password)){
-        return response()->json([
-            'error' => 'Invalid Password'], 401);
-         }
-
-
-        // Generate JWT token
-        $token = JWTAuth::fromUser($user);
+     
 
         return response()->json([
-            'message' => 'User Registered',
+            'message' => 'logged in successfully',
             'user' => $user,
-            'token' => $token
+            'message' => 'Welcome to your dashboard',
         ], 201);
+    }
+
+    public function logout(Request $request)
+    {
+        try {
+            $token = JWTAuth::getToken();
+            if (!$token) {
+                return response()->json(['error' => 'Token not provided'], 400);
+            }
+            JWTAuth::invalidate($token);
+            return response()->json(['message' => 'User logged out successfully'], 200);
+        } catch (\Tymon\JWTAuth\Exceptions\JWTException $e) {
+            return response()->json(['error' => 'Failed to log out Invalid'], 401);
+        }
     }
 
 }
